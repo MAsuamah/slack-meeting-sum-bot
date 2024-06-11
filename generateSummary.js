8e57a0cbca12cba8dd21804e15ccd30f589d3cef
Michelle Asuamah <michelleasuamah@Michelles-MBP-2.lan>
2024-06-11 18:42:22 -0400

    change slack command to download from drive instead of url

---
+++ generateSummary.js
@@ -0,0 +1,21 @@
+import { AssemblyAI } from "assemblyai";
+import 'dotenv/config';
+
+const client = new AssemblyAI({apiKey: process.env.ASSEMBLYAI_API_KEY});
+
+export const generateSummary = async(meetingAudio) => {
+    const params = { audio: meetingAudio }
+    const transcript = await client.transcripts.transcribe(params)
+    const prompt = `Provide a summary of this meeting. Make sure to highlight important topics of the meeting and do so by providing a title for each topic, one sentence briefly describing the topic, 2-3 bullet points summarizing the topic, and a list action items for each topic. 
+                    The format should look like this:
+                    *<topic header>*
+                    _<topic summary>_
+                    <* bullet point list summarizing topic> \n\n
+                    ✅ _Action Items_:
+                    <* list of action items>
+                    `
+    const {response}  = await client.lemur.task({transcript_ids: [transcript.id], prompt})
+    return response
+};
+
+
--- googleDrive.js
+++ googleDrive.js
@@ -1,28 +1,58 @@
 import { google } from 'googleapis';
-import 'dotenv/config'
+import 'dotenv/config';
+import { generateSummary } from './generateSummary.js';
 
 const oauth2Client = new google.auth.OAuth2 (
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     process.env.REDIRECT_URL
-)
+);
 
-oauth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN })
-
-const drive = google.drive({
-    version: 'v3',
-    auth: oauth2Client
-})
+oauth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN });
+const drive = google.drive({version: 'v3', auth: oauth2Client});
 
 const searchFiles = async() => {
     try {
         const response = await drive.files.list({
             q: `sharedWithMe and name contains 'Sprint Planning' and not starred`,
             fields:  'files(id, name)',
+            orderBy: 'createdTime'
         })
-        console.log(response.data);
+        let { files } = response.data
+        for (const file of files) {
+            await downloadAndGetInsight(file);
+        }
+        return response.status;
     } catch(error) {
       console.log(error.message);
+    }
+}
+
+export const downloadAndGetInsight = async(file) => {
+    let id = typeof file !== 'string' ? file.id : file;
+    try {
+        const response = await drive.files.get(
+            {fileId: id, alt: 'media'},
+            {responseType: 'stream'}
+        )
+
+        const chunks = [];
+        response.data.on('data', (chunk) => {chunks.push(chunk);});
+        return new Promise((resolve, reject) => {
+            response.data.on('end', async () => {
+                const buffer = Buffer.concat(chunks);
+                try {
+                    const summary = await generateSummary(buffer);
+                    resolve(summary); // Resolve the promise with summary
+                } catch (error) {
+                    console.error('Error generating summary:', error);
+                    reject(error); // Reject the promise if there's an error
+                }
+            });
+        });
+    } catch(error) {
+        console.error('Error fetching file:', error);
+        throw error;    
     }
 }
 
